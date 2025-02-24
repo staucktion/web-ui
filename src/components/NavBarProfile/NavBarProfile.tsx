d28314c483c7d12cfaa2d78b5c62aa9f3eb35163
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2025-02-24 16:43:27 +0300

    Add logout button

--- src/components/NavBarProfile/NavBarProfile.tsx
+++ src/components/NavBarProfile/NavBarProfile.tsx
@@ -1,62 +1,82 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import redirectWithPost from "../../util/redirectWithPost";
 
 const NavBarProfile: React.FC = () => {
-  const navigate = useNavigate(); //Sayfa yönlendirme için hook
+	const navigate = useNavigate(); //Sayfa yönlendirme için hook
 
-  // Ana sayfaya yönlendirme fonksiyonu
-  const goToHome = () => {
-    navigate("/");
-  };
+	// Ana sayfaya yönlendirme fonksiyonu
+	const goToHome = () => {
+		navigate("/");
+	};
 
-  return (
-    <AppBar
-      position="static"
-      elevation={0}
-      sx={{
-        backgroundColor: "#222", //
-        color: "white",
-      }}
-    >
-      <Toolbar>
-        {/**/}
-        <Typography variant="h6" sx={{ flexGrow: 1, cursor: "pointer" }} onClick={goToHome}>
-          Staucktion
-        </Typography>
+	const handleLogout = () => {
+		redirectWithPost("/auth/logout");
+	};
 
-        {/* Overflow Menu */}
-        <IconButton
-          sx={{ color: "white", mr: 1 }}
-          onClick={() => alert("Overflow menu clicked!")}
-        >
-          <MoreVertIcon />
-        </IconButton>
+	return (
+		<AppBar
+			position="static"
+			elevation={0}
+			sx={{
+				backgroundColor: "#222", //
+				color: "white",
+			}}
+		>
+			<Toolbar>
+				{/**/}
+				<Typography variant="h6" sx={{ flexGrow: 1, cursor: "pointer" }} onClick={goToHome}>
+					Staucktion
+				</Typography>
 
-        {/* Ana Sayfaya Git Butonu */}
-        <Button
-          variant="contained"
-          sx={{
-            textTransform: "none",
-            backgroundColor: "white",
-            color: "black",
-            borderRadius: "9999px",
-            "&:hover": {
-              backgroundColor: "#f0f0f0",
-            },
-          }}
-          onClick={goToHome} //
-        >
-          Home
-        </Button>
-      </Toolbar>
-    </AppBar>
-  );
+				{/* Overflow Menu */}
+				<IconButton sx={{ color: "white", mr: 1 }} onClick={() => alert("Overflow menu clicked!")}>
+					<MoreVertIcon />
+				</IconButton>
+
+				{/* Ana Sayfaya Git Butonu */}
+				<Button
+					variant="contained"
+					sx={{
+						textTransform: "none",
+						backgroundColor: "white",
+						color: "black",
+						borderRadius: "9999px",
+						"&:hover": {
+							backgroundColor: "#f0f0f0",
+						},
+					}}
+					onClick={goToHome} //
+				>
+					Home
+				</Button>
+
+				{/** Logout button */}
+				<Button
+					variant="contained"
+					sx={{
+						textTransform: "none",
+						backgroundColor: "white",
+						color: "black",
+						borderRadius: "9999px",
+						marginLeft: "10px",
+						"&:hover": {
+							backgroundColor: "red",
+						},
+					}}
+					onClick={handleLogout} //
+				>
+					Logout
+				</Button>
+			</Toolbar>
+		</AppBar>
+	);
 };
 
 export default NavBarProfile;
