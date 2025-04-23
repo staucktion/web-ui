2f6a16e87d13eda52bf1fae3dfe72d8161af419d
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2025-04-24 00:33:01 +0300

    Add field to update photo to auction config

--- src/dto/dbConfig/DbConfigDto.ts
+++ src/dto/dbConfig/DbConfigDto.ts
@@ -3,6 +3,7 @@ import BaseResponseDto from "../base/BaseResponseDto";
 interface DbConfigDto extends BaseResponseDto {
 	voter_comission_percentage: number;
 	photographer_comission_percentage: number;
+	photos_to_auction_percentage: number;
 	is_timer_job_active: boolean;
 }
 
