0773122dc21f85608bf4ad5704b1b80dd388993c
DamlaTech <damlakoklu00@gmail.com>
2025-04-24 10:56:16 +0300

    Showing dead photos in the auction UI (without integration

--- src/pages/AdminPanel/AdminPanel.tsx
+++ src/pages/AdminPanel/AdminPanel.tsx
@@ -2,17 +2,31 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import SystemSettings from "../../components/SystemSettings/SystemSettings";
 import AdminUsers from "../../components/AdminUsers/AdminUsers";
+import EndedAuctions from "../../components/EndedAuctions/EndedAuctions";
 
 const AdminPanel: React.FC = () => {
 	return (
 		<Box sx={{ backgroundColor: "#000", color: "#fff", pt: 5, px: 5 }}>
-			<Typography variant="h3" gutterBottom>
-				Admin Panel
-			</Typography>
-			<SystemSettings />
-			<br />
-			<AdminUsers />
-		</Box>
+      <Typography variant="h3" gutterBottom>
+        Admin Panel
+      </Typography>
+
+      <SystemSettings />
+
+      <Box sx={{ mt: 4 }}>
+        <Typography variant="h4" gutterBottom>
+          Users
+        </Typography>
+        <AdminUsers />
+      </Box>
+
+      <Box sx={{ mt: 4 }}>
+        <Typography variant="h4" gutterBottom>
+          Ended Auctions
+        </Typography>
+        <EndedAuctions />
+      </Box>
+    </Box>
 	);
 };
 
