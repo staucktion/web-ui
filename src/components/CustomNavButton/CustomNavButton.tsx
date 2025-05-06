fe628213e9f221ffc2cc3e2345ee0edbc3a8d6b7
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2025-05-06 20:35:47 +0300

    fix responsive error

--- src/components/CustomNavButton/CustomNavButton.tsx
+++ src/components/CustomNavButton/CustomNavButton.tsx
@@ -2,49 +2,51 @@ import React from "react";
 import styled from "styled-components";
 
 interface CustomNavButtonProps {
-  children: React.ReactNode;
-  isActive: boolean;
-  onClick: () => void;
+	children: React.ReactNode;
+	isActive: boolean;
+	onClick: () => void;
 }
 
-const StyledButton = styled.button<CustomNavButtonProps>`
-  position: relative;
-  background: none;
-  border: none;
-  outline: none;
-  cursor: pointer;
+const StyledButton = styled.button<{ $isActive: boolean }>`
+	position: relative;
+	background: none;
+	border: none;
+	outline: none;
+	cursor: pointer;
 
-  font-size: 1.4rem;
-  padding: 10px 20px;
-  text-transform: none;
-  color: black;
-  transition: all 0.3s ease-in-out;
+	font-size: 1.4rem;
+	padding: 10px 20px;
+	color: black;
+	transition: all 0.3s ease-in-out;
 
-  &::after {
-    content: "";
-    position: absolute;
-    left: 20px;
-    right: 20px;
-    bottom: 2px;
-    height: 2px;
-    background-color: black;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5); /* subtle shadow under line */
-    transform: scaleX(${(props) => (props.isActive ? 1 : 0)});
-    transform-origin: center;
-    transition: transform 0.3s ease-in-out;
-  }
+	&::after {
+		content: "";
+		position: absolute;
+		left: 0;
+		bottom: 2px;
+		height: 2px;
+		width: ${({ $isActive }) => ($isActive ? "100%" : "0%")};
+		background-color: black;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+		transition: width 0.3s ease-in-out;
+	}
 
-  &:hover::after {
-    transform: scaleX(1);
-  }
+	&:hover::after {
+		width: 100%;
+	}
+
+	@media (max-width: 600px) {
+		font-size: 1.2rem;
+		padding: 10px 2px;
+	}
 `;
 
 const CustomNavButton: React.FC<CustomNavButtonProps> = ({ children, isActive, onClick }) => {
-  return (
-    <StyledButton onClick={onClick} isActive={isActive}>
-      {children}
-    </StyledButton>
-  );
+	return (
+		<StyledButton onClick={onClick} $isActive={isActive}>
+			{children}
+		</StyledButton>
+	);
 };
 
 export default CustomNavButton;
